4af9101c4da3b744d931191594645a52b991637f
Josh Popp <joshpopp@joshs-mbp-9.win.ad.jhu.edu>
2020-03-05 16:38:50 -0500

    Try pca, add processed data folder

--- hw1/exp_pca.py
+++ hw1/exp_pca.py
@@ -1,21 +1,30 @@
 import numpy as np
+from scipy.linalg import svd
 
 exp_file = "data/exp.data.txt"
 
 
 def load_file(fname):
     expmat = np.loadtxt(exp_file)
-    genes = expmat[1:, 0]
-    inds = expmat[0,1:]
-    expmat = expmat[1:,1:]
-    return inds, genes, expmat
+    return expmat
+
+
+def get_PCs(data, npcs):
+    U, _, _ = svd(expmat, full_matrices=F)
+    return U[:, :npcs]
+
+
+def get_loadings(data, pcs):
+    return np.matmul(data, pcs)
 
 
 def main():
-    expmat, genes, inds = load_file(exp_file)
-    print(expmat[0,:])
-    print(genes)
-    print(inds)
+    expmat = load_file(exp_file)
+    expmat = np.transpose(expmat)  # I want it to be samples x genes
+    U = get_PCs(expmat, 20)
+    np.savetxt("expressionPCs_1_20.txt", U)
+    loadings = get_loadings(expmat, U)
+    np.savetxt("loadings_1_20.txt", loadings)
 
 
 if __name__=="__main__":
