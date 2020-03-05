5fd3c1480d2c218a7633b3327e45cb21dc5d2f9a
Josh Popp <joshpopp@joshs-mbp-9.win.ad.jhu.edu>
2020-03-05 16:50:26 -0500

    Add scree plot

--- hw1/exp_pca.py
+++ hw1/exp_pca.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.linalg import svd
+import matplotlib.pyplot as plt
 
 exp_file = "data/exp.data.txt"
 
@@ -10,19 +11,30 @@ def load_file(fname):
 
 
 def get_PCs(data, npcs):
-    U, _, _ = svd(expmat, full_matrices=F)
-    return U[:, :npcs]
+    U, svals, _ = svd(expmat)
+    evals = [s**2 for s in svals]
+    tot_var = sum(evals)
+    pve = [evals[i]/tot_var for i in range(len(evals))]
+    return U[:, :npcs], pve
 
 
 def get_loadings(data, pcs):
     return np.matmul(data, pcs)
 
 
+def create_scree_plot(pve):
+    plt.plot(range(len(pve)), pve)
+    plt.xlabel('PCs')
+    plt.ylabel('PVE')
+    plt.savefig('scree.png')
+
+
 def main():
     expmat = load_file(exp_file)
     expmat = np.transpose(expmat)  # I want it to be samples x genes
-    U = get_PCs(expmat, 20)
+    U, pve = get_PCs(expmat, 20)
     np.savetxt("expressionPCs_1_20.txt", U)
+    create_scree_plot(pve)
     loadings = get_loadings(expmat, U)
     np.savetxt("loadings_1_20.txt", loadings)
 
