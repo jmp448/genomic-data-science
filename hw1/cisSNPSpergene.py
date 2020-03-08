b843b7f46ca5fa497c7715dcf6e228bf9d923bc9
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-03-08 13:43:35 -0400

    Fix mistake in genelocs v generanges

--- hw1/cisSNPSpergene.py
+++ hw1/cisSNPSpergene.py
@@ -10,25 +10,22 @@ genelocs = np.array(genelocs)
 
 generanges = np.zeros(genelocs.shape)
 generanges[:, 0] = genelocs[:, 0] - 1e6
-generanges[:, 0] = [i if i>=1 else 1 for i in genelocs[:,0]]  # i think they use one-indexing?
+generanges[:, 0] = [i if i>=1 else 1 for i in generanges[:,0]]  # i think they use one-indexing?
 
 chr10len = 133797422
 generanges[:, 1] = genelocs[:, 1] + 1e6
-generanges[:, 1] = [i if i<=chr10len else chr10len for i in genelocs[:,1]]
+generanges[:, 1] = [i if i<=chr10len else chr10len for i in generanges[:,1]]
 
 snpcount = np.zeros(len(generanges), dtype=int)
 startSNP = 0
 for i in range(len(generanges)):
-    print("STARTSNP is %d" % startSNP)
     g = generanges[i]
-    print("Gene %d runs from %d to %d " % (i, g[0], g[1]))
     for j in range(len(snplocs[startSNP:])):
         snploc = snplocs[j]
         if snploc < g[0]:
             startSNP = j
         elif g[0] <= snploc < g[1]:
             snpcount[i] += 1
-            print("FOUND SNP FOR GENE %d")
         else:
             break
 
