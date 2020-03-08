8917fffe40e0ecd099986f39fd5cb56a21105fc3
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-03-07 22:04:09 -0500

    Cleanup

--- hw1/create_covfiles.sh
+++
@@ -1,7 +0,0 @@
-covfile=/work-zfs/abattle4/lab_data/GTEx_v8_eqtl_practice/matrix_eqtl/Whole_Blood.v8.covariates.txt
-
-# Covariate file 1: genotype PCs and sex only
-cat $covfile | head -n 6 > cov1.txt
-cat $covfile | tail -n 1 >> cov1.txt
-
-# Covariate file 2: genotype PCs, sex, and 5 expression PCs
--- hw1/slurmq3.sh
+++ hw1/slurmq3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=q3
 #SBATCH --time=0:20:0
-#SBATCH --output=q3.out
-#SBATCH --error=q3.err
+#SBATCH --output=slurm_output/q3.out
+#SBATCH --error=slurm_output/q3.err
 #SBATCH --mem=25G
 #SBATCH --partition=shared
 #SBATCH --mail-user=jpopp4@jhu.edu
