4807a66b37b6087050d67a5c6e2eee968522f2c0
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-03-08 14:51:35 -0400

    slurm SNP counting

---
+++ hw1/slurmSNPcount.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=countSNPcands
+#SBATCH --time=0:20:0
+#SBATCH --output=slurm_output/SNPcount.out
+#SBATCH --error=slurm_output/SNPcount.err
+#SBATCH --mem=5G
+#SBATCH --mail-user=jpopp4@jhu.edu
+
+module load python/3.7-anaconda
+
+python cisSNPSpergene.py
