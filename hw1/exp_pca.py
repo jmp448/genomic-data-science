cb999a626b00212117f9ca5871730c9a78ebbd91
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-03-20 13:41:20 -0400

    Quick fix for scree

--- hw1/exp_pca.py
+++ hw1/exp_pca.py
@@ -24,7 +24,7 @@ def get_loadings(data, pcs):
 
 
 def create_scree_plot(pve):
-    plt.scatter(range(len(pve)), pve*100)
+    plt.scatter(np.array(range(len(pve)))+np.ones(len(pve), dtype=int), pve*100)
     plt.xlabel('PCs')
     plt.ylabel('PVE')
     plt.savefig('scree.png')
