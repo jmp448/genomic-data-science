2b3a2e65e06ffd4cf08b9e33ca685fd6c26f47d7
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-03-20 15:50:34 -0400

    redo with the last batch

--- hw1/slurmy/q4.sh
+++ hw1/slurmy/q4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=q4
-#SBATCH --time=0:30:0
+#SBATCH --time=0:10:0
 #SBATCH --output=slurm_output/q4.out
 #SBATCH --error=slurm_output/q4.err
 #SBATCH --mail-user=jpopp4@jhu.edu
@@ -9,9 +9,9 @@
 
 module load R
 
-srun Rscript eqtl_cov_input.R "cov1" 0
-srun Rscript eqtl_cov_input.R "cov2" 1
-srun Rscript eqtl_cov_input.R "cov3" 2
-srun Rscript eqtl_cov_input.R "cov4" 3
-srun Rscript eqtl_cov_input.R "cov5" 4
+# srun Rscript eqtl_cov_input.R "cov1" 0
+# srun Rscript eqtl_cov_input.R "cov2" 1
+# srun Rscript eqtl_cov_input.R "cov3" 2
+# srun Rscript eqtl_cov_input.R "cov4" 3
+srun Rscript eqtl_cov_input.R "cov5" 0
 wait
