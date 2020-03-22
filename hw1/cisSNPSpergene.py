3eaf6e8c51d399c066d3e57680227636244094d2
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-03-21 20:53:35 -0400

    update SNP counts for filtering

--- hw1/cisSNPSpergene.py
+++ hw1/cisSNPSpergene.py
@@ -1,9 +1,26 @@
 import numpy as np
 
+# load snp file
 snpfile = "/work-zfs/abattle4/lab_data/GTEx_v8_eqtl_practice/matrix_eqtl/Whole_Blood.v8.snp_location.chr10.txt"
 snplocs = np.loadtxt(snpfile, dtype=int, skiprows=1, usecols=2)
 snplocs = np.array(snplocs)
 
+# select from this array only the snps that passed the MAF threshold
+maf_file = open("snps.passed", "r")
+fl = maf_file.readlines()
+maf_snps = [f.strip() for f in fl[1:]]
+
+allsnp_file = open(snpfile, "r")
+fl = allsnp_file.readlines()
+all_snps = [f.strip().split()[0] for f in fl[1:]]
+
+assert(len(all_snps) == len(snplocs))
+assert(len(maf_snps) == 339919)
+
+_, inds, _ = np.intersect1d(all_snps, maf_snps, return_indices=True)
+snplocs = np.take(snplocs, inds)
+
+# load gene file
 genefile = "/home-2/jpopp4@jhu.edu/work/josh/genomic-data-science/hw1/data/chr10genelocs.txt"
 genelocs = np.loadtxt(genefile, dtype=int, skiprows=0, usecols=(2,3))
 genelocs = np.array(genelocs)
@@ -29,4 +46,4 @@ for i in range(len(generanges)):
         else:
             break
 
-np.savetxt("snpcounts.chr10.txt", snpcount, fmt="%d")
+np.savetxt("snpcounts.chr10.maffiltered.txt", snpcount, fmt="%d")
