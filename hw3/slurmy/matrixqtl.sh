38106a4bc548251969da9d75bd1c1d1b25f4ab3e
jmp448 <joshpopp97@gmail.com>
2020-05-04 19:18:00 -0400

    with slurm commands

---
+++ hw3/slurmy/matrixqtl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=matrixeqtl
+#SBATCH --time=1:0:0
+#SBATCH --output=slurm_output/qtl.out
+#SBATCH --error=slurm_output/qtl.err
+#SBATCH --mail-user=jpopp4@jhu.edu
+#SBATCH --partition=express
+#SBATCH --mem=50G
+
+module load R
+
+Rscript ../code/transeqtl.R
